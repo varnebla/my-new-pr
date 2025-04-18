db0ea0987ff5de990ea24a427f4e7d22aa88a180
Victor Arnedo Blanco <victor@MacBook-Air-de-Victor.local>
2025-04-18 20:45:24 +0200

    start project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vue from "@astrojs/vue";
+import tailwindcss from "@tailwindcss/vite";
+
+import auth from "auth-astro";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue(), auth()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  output:'server',
+  adapter: vercel(),
+});
